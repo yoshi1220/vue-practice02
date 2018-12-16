4d8559400339605d81d8b4276dc009b8c51bc6f6
yoshi1220 <temari@themis.ocn.ne.jp>
2018-12-16 22:56:13 +0900

    v-for, v-on, v-modelのサンプル追加

--- app1/main.js
+++ app1/main.js
@@ -2,5 +2,17 @@ var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello vue.js!'
+  },
+  methods: {
+    handleClick: function(event) {
+      alert(event.target)
+    }
+  }
+})
+
+var app2 = new Vue({
+  el: '#app2',
+  data: {
+    list: ['りんご', 'みかん', 'いちご']
   }
 })
